df042746601fc03023ac972f5feb7337894eb440
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-15 15:04:37 +0100

    Integrate the routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Sign from "./pages/Sign";
+import User from "./pages/User";
+
 function App() {
-  return (
-      <>app</>
-  );
+    return (
+        <>
+          {/* navbar */}
 
+          {/* 
+            routes
+              /
+              /sign 
+              /user
+          */}
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" end element={<Home />} />
+              <Route path="/sign" end element={<Sign />} />
+              <Route path="/user" end element={<User />} />
+              <Route path="/error" end element={<Error />} />
+            </Routes>
+          </BrowserRouter>
+          {/* outlet */}
+          <Outlet />
+          
+          {/* footer */}
+        </>
+    );
+}
 
-  }
+export default App;
