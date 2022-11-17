6b0fcca05710bc46f68278e3bb0959ce24b7a7de
BELAIDYasser <yasser.bel93@gmail.com>
2022-11-17 11:54:51 +0100

    Create Error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,6 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+import './../assets/css/layout/_error.css';
+
 function Error() {
     return (
-        <div>Error</div>
+        <div className="main">
+            <p className="error-code"><FontAwesomeIcon icon={faRobot} /><br /> ERROR <span>404 </span></p>
+            <p className="error-description">Sorry! this page isn't avaliable.</p>
+            <p className="redirect">click  <a href="/">here</a> to go to the home page</p>
+        </div>
     )
 }
 
